dd8dedcde48466e4fd9ebc1d8b98c28ac5f8d6b4
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-04-01 15:42:48 +0200

    amélioration footer, ajout fonctionnalité button scroll to top, row évènements, posts its blocs pour les news

--- Application_web/js/script.js
+++ Application_web/js/script.js
@@ -1,71 +1,21 @@
-//Based on the Scroller function from @sallar
-var $content = $("header .content"),
-  $blur = $("header .overlay"),
-  wHeight = $(window).height();
+mybutton = document.getElementById("myBtn");
 
-$(window).on("resize", function () {
-  wHeight = $(window).height();
-});
-
-window.requestAnimFrame = (function () {
-  return (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 60);
-    }
-  );
-})();
-
-function Scroller() {
-  this.latestKnownScrollY = 0;
-  this.ticking = false;
-}
-
-Scroller.prototype = {
-  init: function () {
-    window.addEventListener("scroll", this.onScroll.bind(this), false);
-    $blur.css(
-      "background-image",
-      $("header:first-of-type").css("background-image")
-    );
-  },
-
-  onScroll: function () {
-    this.latestKnownScrollY = window.scrollY;
-    this.requestTick();
-  },
-
-  requestTick: function () {
-    if (!this.ticking) {
-      window.requestAnimFrame(this.update.bind(this));
-    }
-    this.ticking = true;
-  },
-
-  update: function () {
-    var currentScrollY = this.latestKnownScrollY;
-    this.ticking = false;
-
-    var slowScroll = currentScrollY / 2,
-      blurScroll = currentScrollY * 2,
-      opaScroll = 1.4 - currentScrollY / 400;
-    if (currentScrollY > wHeight) $("nav").css("position", "fixed");
-    else $("nav").css("position", "absolute");
-
-    $content.css({
-      transform: "translateY(" + slowScroll + "px)",
-      "-moz-transform": "translateY(" + slowScroll + "px)",
-      "-webkit-transform": "translateY(" + slowScroll + "px)",
-      opacity: opaScroll,
-    });
-
-    $blur.css({
-      opacity: blurScroll / wHeight,
-    });
-  },
+window.onscroll = function () {
+  scrollFunction();
 };
 
-var scroller = new Scroller();
-scroller.init();
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
