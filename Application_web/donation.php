f3e712ac8f16e7f601ba5dc14ea92eae0ea7b17e
aurelie661 <boutaurel59@gmail.com>
2021-04-19 09:47:41 +0200

    donate

--- Application_web/donation.php
+++ Application_web/donation.php
@@ -7,8 +7,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/donation.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="commondocs/commons.css">
+    <link rel="stylesheet" href="css/donation.css">
 
 
 </head>
