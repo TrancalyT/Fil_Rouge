b6cdd09a5dfb85299fb803b33d3ed14f862731cb
aurelie661 <boutaurel59@gmail.com>
2021-04-27 21:54:16 +0200

    maj profil

--- Application_web/profil.php
+++ Application_web/profil.php
@@ -27,16 +27,17 @@
     </header>
     <!-------------------------------------CORP--------------------------------------------->
     <div class="contenant">
-        <p><a href="">Profil de .........</a></p>
+    <p><a href="">Profil de Liyahhhhhh</a></p> 
         <div class="grid-container">
+        
             <div class="infos">
                 <form class="formulaire" method="POST" action="">
                     <div>
                         <h6 style="text-align: center;font-family:'lato','sans-serif';">Modifier mes infos</h6>
-                        <input type="text" name="newpseudo" placeholder="pseudo" /><br /><br />
-                        <input type="email" name="newmail" placeholder="mail" /><br /><br />
-                        <input type="password" name="newmdp1" placeholder="Mot de passe" /><br /><br />
-                        <input type="password" name="newmdp2" placeholder="Confirmer le mot de passe" /><br /><br />
+                        <input type="text" name="newpseudo" placeholder=" pseudo" /><br /><br />
+                        <input type="email" name="newmail" placeholder=" mail" /><br /><br />
+                        <input type="password" name="newmdp1" placeholder=" Mot de passe" /><br /><br />
+                        <input type="password" name="newmdp2" placeholder=" Confirmer le mot de passe" /><br /><br />
                         <input style="font-family:'lato','sans-serif';" type="button" class="button4" value="Mettre à jour mon profil !" ; />
                     </div>
                 </form>
@@ -46,11 +47,11 @@
             <div class="coordonnees">
                 <form class="formulaire" method="POST" action="">
                     <div>
-                        <h6 style="text-align: center;font-family:'lato','sans-serif';">Modifier mes coordonnees</h6>
-                        <input type="text" size="35" maxlength="100" name="newAdresse" placeholder="Adresse" /><br /><br />
-                        <input type="text" name="newville" placeholder="Ville" /><br /><br />
-                        <input type="number" name="newCP" placeholder="Code Postal" /><br /><br />
-                        <input type="phone" name="newTelephone" placeholder="Téléphone" /><br /><br />
+                        <h6 style="text-align: center;font-family:'lato','sans-serif';">Modifier mes coordonnées</h6>
+                        <input type="text" size="35" maxlength="100" name="newAdresse" placeholder=" Adresse" /><br /><br />
+                        <input type="text" name="newville" placeholder=" Ville" /><br /><br />
+                        <input type="number" name="newCP" placeholder=" Code Postal" /><br /><br />
+                        <input type="phone" name="newTelephone" placeholder=" Téléphone" /><br /><br />
                         <input style="font-family:'lato','sans-serif';" type="button" class="button4" value="Mettre à jour mes coordonnées !" ; />
                     </div>
                 </form>
