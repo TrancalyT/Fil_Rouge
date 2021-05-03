378e2ee7111663c6813334ebeef96995fb8ed4d5
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-05-03 10:12:47 +0200

    modif tables DB

--- Application_web/DB/museum.sql
+++ Application_web/DB/museum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 30 avr. 2021 à 13:57
+-- Généré le : lun. 03 mai 2021 à 10:11
 -- Version du serveur :  10.4.17-MariaDB
 -- Version de PHP : 8.0.2
 
@@ -24,6 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `boutique`
+--
+
+CREATE TABLE `boutique` (
+  `ID` int(11) NOT NULL,
+  `NOM` varchar(100) NOT NULL,
+  `DATE` date NOT NULL,
+  `PRIX` float NOT NULL,
+  `QUANTITE` int(11) NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `PHOTO` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `donation`
+--
+
+CREATE TABLE `donation` (
+  `ID` int(11) NOT NULL,
+  `DATE` date NOT NULL,
+  `MONTANT` float NOT NULL,
+  `MAIL` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `expo`
+--
+
+CREATE TABLE `expo` (
+  `ID` int(11) NOT NULL,
+  `NOM` varchar(255) NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  `IMAGE` blob NOT NULL,
+  `EVAL` varchar(3) NOT NULL,
+  `CONTENU` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `forum_message`
 --
 
@@ -38,27 +82,29 @@ CREATE TABLE `forum_message` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `forum_validate`
+-- Structure de la table `forum_topic`
 --
 
-CREATE TABLE `forum_validate` (
+CREATE TABLE `forum_topic` (
   `ID` int(11) NOT NULL,
-  `TEXT` int(11) NOT NULL,
-  `STARS` int(5) NOT NULL,
-  `SIGNATURE` varchar(20) NOT NULL
+  `TOPIC` varchar(255) NOT NULL,
+  `AUTHOR` varchar(60) NOT NULL,
+  `MESSAGE` text NOT NULL,
+  `DATE` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `goldbook_attente`
+-- Structure de la table `goldbook`
 --
 
-CREATE TABLE `goldbook_attente` (
+CREATE TABLE `goldbook` (
   `ID` int(5) NOT NULL,
   `TEXT` int(11) NOT NULL,
   `STARS` int(5) NOT NULL,
-  `SIGNATURE` varchar(20) NOT NULL
+  `SIGNATURE` varchar(20) NOT NULL,
+  `VALIDATION` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -82,12 +128,32 @@ CREATE TABLE `user` (
   `BOOK` varchar(100) NOT NULL,
   `MUSIC` varchar(100) NOT NULL,
   `SPORT` varchar(100) NOT NULL,
-  `VG` varchar(100) NOT NULL
+  `VG` varchar(100) NOT NULL,
+  `BIO` text NOT NULL,
+  `AVATAR` blob NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `boutique`
+--
+ALTER TABLE `boutique`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `donation`
+--
+ALTER TABLE `donation`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `expo`
+--
+ALTER TABLE `expo`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Index pour la table `forum_message`
@@ -96,15 +162,15 @@ ALTER TABLE `forum_message`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Index pour la table `forum_validate`
+-- Index pour la table `forum_topic`
 --
-ALTER TABLE `forum_validate`
+ALTER TABLE `forum_topic`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Index pour la table `goldbook_attente`
+-- Index pour la table `goldbook`
 --
-ALTER TABLE `goldbook_attente`
+ALTER TABLE `goldbook`
   ADD PRIMARY KEY (`ID`);
 
 --
@@ -118,21 +184,39 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `boutique`
+--
+ALTER TABLE `boutique`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `donation`
+--
+ALTER TABLE `donation`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `expo`
+--
+ALTER TABLE `expo`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `forum_message`
 --
 ALTER TABLE `forum_message`
   MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `forum_validate`
+-- AUTO_INCREMENT pour la table `forum_topic`
 --
-ALTER TABLE `forum_validate`
+ALTER TABLE `forum_topic`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `goldbook_attente`
+-- AUTO_INCREMENT pour la table `goldbook`
 --
-ALTER TABLE `goldbook_attente`
+ALTER TABLE `goldbook`
   MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT;
 
 --
