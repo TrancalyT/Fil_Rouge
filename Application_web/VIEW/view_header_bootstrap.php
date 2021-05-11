431a6a48323ba325bb99409bb1c65cbb619790d4
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-05-11 14:14:28 +0200

    ajout controller user

--- Application_web/VIEW/view_header_bootstrap.php
+++ Application_web/VIEW/view_header_bootstrap.php
@@ -9,13 +9,17 @@ function callHeader(string $title, string $css)
 
     <head>
         <title><?= $title ?></title>
-        <link rel="shortcut icon" href="images/icon-index.png">
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" > -->
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <link rel="shortcut icon" href="images/icon-index.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
         <link rel="stylesheet" href="commondocs/commons.css">
         <link rel="stylesheet" href=<?= $css ?>>
 
