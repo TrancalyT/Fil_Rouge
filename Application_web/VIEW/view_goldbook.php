2464f79fc506450358751b17cdd92219cdecc048
Anthony REINA aka Toodle <anthonyreina.pro@gmail.com>
2021-05-20 20:50:09 +0200

    Controller Goldbook : OK

--- Application_web/VIEW/view_goldbook.php
+++ Application_web/VIEW/view_goldbook.php
@@ -1,27 +1,29 @@
 <?php
 
-function callGoldBookConnected($id)
+function callGoldBookConnected($id, $avis, $messageError)
 {
 ?>
 
 <div class="form_livreor">
-    <form action="" method="post" class="col g-3 justify-content form-info">
+    <form action="livreor.php" method="post" class="col g-3 justify-content form-info">
       <div class="container-lg">
+      <span class="fs-6 fst-italic text-danger"><?php echo $messageError["messageError"] ?></span>
+      <span class="fs-6 fst-italic text-danger"><?php echo $messageError["messageErrSignature"] ?></span>
         <div class="mb-3">
           <label for="Message" class="form-label">LAISSEZ NOUS VOTRE AVIS : </label>
           <div class="input-group">
-            <textarea class="form-control" id="Message" rows="8"></textarea>
+            <textarea class="form-control" id="Message" name="avis" rows="8" required><?php echo $avis ?></textarea>
           </div>
           <div class="notif">
             <p class="text">(*) Votre commentaire est soumis à modération avant diffusion</p>
           </div>
           <div class="rating">
             <div class="stars">
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
-              <i class="fa fa-star"></i>
+                <input name="5stars" id="5star" type="radio"></a><label for="5star">★</label>
+                <input name="4stars" id="4star" type="radio"></a><label for="4star">★</label>
+                <input name="3stars" id="3star" type="radio"></a><label for="3star">★</label>
+                <input name="2stars" id="2star" type="radio"></a><label for="2star">★</label>
+                <input name="1stars" id="1star" type="radio"></a><label for="1star">★</label>
             </div>
           </div>
         </div>
@@ -34,7 +36,7 @@ function callGoldBookConnected($id)
           </div>
         </div>
         <div class="mb-3 text-center">
-          <button type="submit" class="buttonmain">Envoyer</button>
+          <button type="submit" class="buttonmain" name="valider" value="valider">Envoyer</button>
         </div>
       </div>
     </form>
@@ -43,7 +45,7 @@ function callGoldBookConnected($id)
 <?php
 }
 
-function callGoldBookUnonnected()
+function callGoldBookUnconnected()
 {
 ?>
 
--- Application_web/livreor.php
+++ Application_web/livreor.php
@@ -12,13 +12,52 @@ callMainTitle("Livre d'or");
 $_SESSION['user_id'] = 4;
 $_SESSION['user_name'] = 'Marlon';
 
+$messageError = [
+  "messageErrSignature" => "",
+  "messageError" => ""];
+
+$regSignature = "#[a-z]{1,20}#i"; // REGEX à préciser
+
+if (isset($_REQUEST['5stars'])){
+  @$stars = 5;
+} else if (isset($_REQUEST['4stars'])){
+  @$stars = 4;
+} else if (isset($_REQUEST['3stars'])){
+  @$stars = 3;
+} else if (isset($_REQUEST['2stars'])){
+  @$stars = 2;
+} else if (isset($_REQUEST['1stars'])){
+  @$stars = 1;
+}
+@$avis = $_REQUEST['avis'];
+@$signature = $_REQUEST['signature'];
+@$valider= $_REQUEST["valider"];
+
+if (isset($valider)){
+
+  if(isset($avis) && !empty($avis) 
+  && isset($signature) && !empty($signature) 
+  && isset($stars) && !empty($stars) ){
+
+      if (!preg_match($regSignature, $signature)){
+          $messageError["messageErrSignature"] = "Signature incorrect";
+      }
+      if (preg_match($regSignature, $signature)){
+          header("Location:dbGoldbook.php?avis=$avis&signature=$signature&stars=$stars"); // CREER process écriture sur DB
+      }
+
+  } else {
+      $messageError["messageError"] = "Veuillez saisir et remplir les informations manquantes";
+  }
+}
+
 if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){
 
-callGoldBookConnected($_SESSION['user_name']);
+callGoldBookConnected($_SESSION['user_name'], $avis, $messageError);
 
 } else {
 
-callGoldBookUnonnected();
+callGoldBookUnconnected();
 
 }
 
