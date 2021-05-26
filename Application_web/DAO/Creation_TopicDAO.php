9221c92e7d59260980e757afcb686ae67a24bc98
aurelie661 <boutaurel59@gmail.com>
2021-05-26 15:01:56 +0200

    DAO

---
+++ Application_web/DAO/Creation_TopicDAO.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . "/../MODEL/Creation_topic.php");
+require_once(__DIR__ . "/CommonDAO.php");
+require_once(__DIR__ . "/../EXCEPTIONS/ForumDAOException.php");
+
+class Creation_TopicDAO extends Connection{
+    
+}
---
+++ Application_web/DAO/Forum_messageDAO.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . "/../MODEL/Forum_message.php");
+require_once(__DIR__ . "/CommonDAO.php");
+require_once(__DIR__ . "/../EXCEPTIONS/ForumDAOException.php");
+
+class Forum_messageDAO extends Connection{
+    
+}
